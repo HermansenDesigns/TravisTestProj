c2dcfeec9eef658cc1bde31afa79314fcb5988ec
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-20 23:27:29 +0100

    Added Subtraction and test

--- TravisTestProj/TravisTestProj.Unit.Test/CalculatorUnitTest.cs
+++ TravisTestProj/TravisTestProj.Unit.Test/CalculatorUnitTest.cs
@@ -11,5 +11,12 @@ namespace TravisTestProj.Unit.Test
             var uut = new Calculator();
             Assert.Equal(4, uut.Add(2, 2));
         }
+
+        [Fact]
+        public void Calculator_Subtraction2Minus3_ReturnsMinus1()
+        {
+            var uut = new Calculator();
+            Assert.Equal(-1, uut.Subtraction(2, 2));
+        }
     }
 }
